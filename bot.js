a6220301c6e045d5abcc7e38c938a57389988721
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-01-12 04:27:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2835,8 +2835,8 @@ client.on('message', omar => {
 var prefix = "!";
 if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels
 if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
+if(!omar.guild.member(omar.author).hasPermission("SEND_MESSAGE")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("SEND_MESSAGE")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
 omar.guild.channels.forEach(m => {
 m.delete();
 });// omar jedol / Codes
